6bfdbac7776b9a9318c32acf8428c42de3e5b1f7
Nirmal Lankathilaka <nir3al@gmail.com>
2015-09-20 19:50:37 +0530

    RunAway game in /apps/

---
+++ app/runaway/js/main.js
@@ -0,0 +1,9 @@
+var SideScroller = SideScroller || {};
+
+SideScroller.game = new Phaser.Game(790, 420, Phaser.AUTO, '');
+
+SideScroller.game.state.add('Boot', SideScroller.Boot);
+SideScroller.game.state.add('Preload', SideScroller.Preload);
+SideScroller.game.state.add('Game', SideScroller.Game);
+
+SideScroller.game.state.start('Boot');
