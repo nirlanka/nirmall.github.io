06db477cd8ed84be5e2884a1298831b41bbf9388
Nirmal Lankathilaka <nir3al@gmail.com>
2015-09-22 10:35:40 +0530

    Level bug (swapped)

--- app/runaway/js/Game2.js
+++ app/runaway/js/Game2.js
@@ -7,7 +7,7 @@ SideScroller.Game.prototype = {
       this.game.time.advancedTiming = true;
     },
   create: function() {
-    this.map = this.game.add.tilemap('level2');
+    this.map = this.game.add.tilemap('level1');
 
     //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
     this.map.addTilesetImage('tiles_spritesheet', 'gameTiles');
